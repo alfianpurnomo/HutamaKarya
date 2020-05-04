825722e319dc76189100a91c3465b1b679772d41
alfianpurnomo <alfian.pacul@gmail.com>
2020-05-04 17:22:58 +0700

    data susulan

---
+++ data_user_employee.sql
@@ -0,0 +1,5 @@
+INSERT INTO `t_data_user` VALUES (1019,'Ranto','Tumangger','HKEPC2001','$2y$10$VpsrcrqIU0TS6oMtfw2mWOZWjT16AayjvH1ZKFdjYlVuuerKogrcy','ranto.tumangger@hk.com','',3,NULL,1,NULL,NULL,'2020-05-04 17:12:44',0,'inspinia'),(1020,'Anggi','Pradikna','HKEPC1911','$2y$10$fW/YZ3682HlQ9L.DTJWdbe1.PSa4LXh7AxEN.OIduOVeYZeV0R/D2','anggi.pradikna@hk.com','',3,NULL,1,NULL,NULL,NULL,0,'inspinia'),(1021,'Ines ','Widhi Astuti','HKEPC1912','$2y$10$rZUGg03Wxa2dzd34zbRf/eAu316K811QYnS2T9/3Ifkfoq6AHv7de','ines.widhi.astuti@hk.com','',3,NULL,1,NULL,NULL,NULL,0,'inspinia');
+INSERT INTO `master_employee` VALUES ('798c4562-8dec-11ea-9971-a20a4b5be469','Ranto','Tumangger','HKEPC2001',11,'L',1,13,'983c6aa0-5e1c-11ea-a74f-929740789a9a','ranto.tumangger@hk.com','',NULL,'','','','','','',1019),
+('63184eb8-8def-11ea-9971-a20a4b5be469','Anggi','Pradikna','HKEPC1911',11,'P',1,8,'982ad9de-5e1c-11ea-a74f-929740789a9a','anggi.pradikna@hk.com','',NULL,'','','','','','',1020),
+('04221ca8-8df0-11ea-9971-a20a4b5be469','Ines ','Widhi Astuti','HKEPC1912',11,'P',1,17,'984e9306-5e1c-11ea-a74f-929740789a9a','ines.widhi.astuti@hk.com','',NULL,'','','','','','',1021);
+
